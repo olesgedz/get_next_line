4cc7d34c525eb583a80ba2bee106f1de466b0eef
Jeromy Black balaq <jblack-b@e3r7p3.21-school.ru>
2018-12-12 17:30:10 +0300

    efghg

--- main.c
+++ main.c
@@ -6,21 +6,37 @@
 /*   By: jblack-b <jblack-b@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/12 16:20:41 by jblack-b          #+#    #+#             */
-/*   Updated: 2018/12/12 16:30:39 by jblack-b         ###   ########.fr       */
+/*   Updated: 2018/12/12 17:30:02 by jblack-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 
-/*
-int		get_next_line(const int fd, char **line)
+#define BUF_SIZE 1
+
+int		get_next_line(const int fd)
 {
+	int		ret;
+	char	buf[BUF_SIZE + 1];
 
+	while ((ret = read(fd, buf, BUF_SIZE)))
+	{
+		if (buf[0] == '\n')
+			return (0);
+		buf[ret] = '\0';
+		ft_putstr(buf);
+	}
+	return (0);
 }
-*/
-int	main()
+
+int		main(int argc, char **argv)
 {
-	ft_putstr("hello");
+	int		fd;
+
+	fd = open(argv[1], O_RDONLY);
+	get_next_line(fd);
 	return (0);
 }
