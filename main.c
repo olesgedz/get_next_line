3f4bf955b89ca015b2857a890babb75932a7222c
Jeromy Black balaq <jblack-b@e3r7p3.21-school.ru>
2018-12-16 21:00:29 +0300

    asd

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jblack-b <jblack-b@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/12 16:20:41 by jblack-b          #+#    #+#             */
-/*   Updated: 2018/12/16 18:29:22 by jblack-b         ###   ########.fr       */
+/*   Updated: 2018/12/16 21:00:24 by jblack-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,15 +31,25 @@ int		ft_check_buff(char *buf, int ret)
 	}
 	return (0);
 }
+
 int		get_next_line(const int fd, char **line)
 {
 	int		ret;
 	char	buf[BUF_SIZE + 1];
 	static t_list *lst;
+	size_t i;
 
-	lst = malloc((sizeof(t_list)));
+	i = 0;
+	lst = malloc(sizeof(t_list));
 	ret = read(fd, buf, BUF_SIZE);
-	ft_putstr(lst->content);
+	buf[ret] = '\0';
+	ft_putstr(buf);
+	lst->content = malloc(sizeof(ret * sizeof(char)));
+	//*(char *)(lst->content) = 'L';
+	//printf("%c", *(char *)(lst->content));
+	//ft_memcpy((char *)lst->content, buf, ret - 1);
+	//ft_putstr(lst->content);
+	//*line = lst->content;
 	return (0);
 }
 
@@ -47,9 +57,13 @@ int		main(int argc, char **argv)
 {
 	int		fd;
 	char **a;
-
+	if (argc != 2)
+	{
+		ft_putstr("ERROR");
+		return (NULL);
+	}
 	fd = open(argv[1], O_RDONLY);
 	get_next_line(fd, a);
-	get_next_line(fd, a);
+	//ft_putstr(*a);
 	return (0);
 }
