13e3279dfecba5065d74094d11957408e6089cc8
Jeromy Black balaq <jblack-b@e3r7p3.21-school.ru>
2018-12-21 20:59:07 +0300

    asdawdaf

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jblack-b <jblack-b@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/12 16:20:41 by jblack-b          #+#    #+#             */
-/*   Updated: 2018/12/20 21:53:24 by jblack-b         ###   ########.fr       */
+/*   Updated: 2018/12/21 20:58:53 by jblack-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,17 +35,50 @@ static t_list			*get_correct_file(t_list **file, int fd)
 	return (tmp);
 }
 
+/*int		get_next_line(const int fd, char **line)
+{
+	int						ret;
+	char					buf[BUF_SIZE + 1];
+	static				t_list *lst;
+	//t_list 							*lst;
+	int						i;
+
+	//lst = file;
+	if (!lst)
+		MALLOC_CHECK(lst = ft_lstnew("", fd));
+	while ((ret = read(fd, buf, BUF_SIZE)))
+	{
+		if (!(lst->content))
+			MALLOC_CHECK(lst->content = malloc(sizeof(char) * (ret + 1)));
+		buf[ret] = '\0';
+		lst->content = ft_strjoin((char *)lst->content, buf);
+		if (ft_strchr((char *)lst->content, '\n'))
+			break ;
+	}
+	if (ret == 0 && !*(char *)lst->content)
+		return (0);
+	*line = ft_strsub((char *)lst->content, 0,\
+	ft_strchr((char *)lst->content, '\n') - (char *)lst->content);
+	i = ft_strchr((char *)lst->content, '\n') - (char *)lst->content;
+	(i < (int)ft_strlen((char *)lst->content))\
+	? lst->content += (i + 1) : ft_strclr((char *)lst->content);
+	ft_putstr(*line);
+	return (1);
+}*/
+
+
 int		get_next_line(const int fd, char **line)
 {
 	int						ret;
 	char					buf[BUF_SIZE + 1];
 	static				t_list *file;
-	t_list 							*lst;
+	char				*temp;
 	int						i;
+	t_list *lst;
 
-	if ((fd < 0 || line == NULL || read(fd, buf, 0) < 0))
-		return (-1);
-	lst = get_correct_file(&file, fd);
+	if (!file)
+		MALLOC_CHECK(file = ft_lstnew("", fd));
+	lst = file;
 	/*if (!lst)
 		MALLOC_CHECK(lst = ft_lstnew("", fd));*/
 	while ((ret = read(fd, buf, BUF_SIZE)))
@@ -73,11 +106,14 @@ int		main(int argc, char **argv)
 	int		fd2;
 	char	*line;
 
-	if (argc < 2)
+
+if (argc < 2)
 	{
 		ft_putstr("ERROR");
 		return (0);
 	}
+	if (argc > 2)
+	{
 	fd = open(argv[1], O_RDONLY);
 	fd = open(argv[2], O_RDONLY);
 	get_next_line(fd, &line);
@@ -92,10 +128,15 @@ int		main(int argc, char **argv)
 	ft_putstr(line);
 	get_next_line(fd2, &line);
 	ft_putstr(line);
-	/*while (get_next_line(fd, &line))
+}
+else
+{
+	fd = open(argv[1], O_RDONLY);
+	while (get_next_line(fd, &line))
 	{
 		ft_putstr(line);
 		ft_putstr("\n");
-	}*/
+	}
+}
 	return (0);
 }
