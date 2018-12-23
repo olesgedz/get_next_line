e9bb815da6591593a95680ce68c4c3e372f0fea8
Jeromy Black balaq <jblack-b@e3r7p3.21-school.ru>
2018-12-23 22:16:05 +0300

    eafd

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jblack-b <jblack-b@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/12 16:20:41 by jblack-b          #+#    #+#             */
-/*   Updated: 2018/12/23 20:29:26 by jblack-b         ###   ########.fr       */
+/*   Updated: 2018/12/23 22:15:51 by jblack-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,54 +31,39 @@ static t_list			*get_correct_file(t_list **file, int fd)
 	}
 	tmp = ft_lstnew("\0", fd);
 	ft_lstadd(file, tmp);
-	tmp = *file;
+	//tmp = *file;
 	return (tmp);
 }
 
-/*int		get_next_line(const int fd, char **line)
-{
-	int						ret;
-	char					buf[BUF_SIZE + 1];
-	static				t_list *lst;
-	//t_list 							*lst;
-	int						i;
-
-	//lst = file;
-	if (!lst)
-		MALLOC_CHECK(lst = ft_lstnew("", fd));
-	while ((ret = read(fd, buf, BUF_SIZE)))
-	{
-		if (!(lst->content))
-			MALLOC_CHECK(lst->content = malloc(sizeof(char) * (ret + 1)));
-		buf[ret] = '\0';
-		lst->content = ft_strjoin((char *)lst->content, buf);
-		if (ft_strchr((char *)lst->content, '\n'))
-			break ;
-	}
-	if (ret == 0 && !*(char *)lst->content)
-		return (0);
-	*line = ft_strsub((char *)lst->content, 0,\
-	ft_strchr((char *)lst->content, '\n') - (char *)lst->content);
-	i = ft_strchr((char *)lst->content, '\n') - (char *)lst->content;
-	(i < (int)ft_strlen((char *)lst->content))\
-	? lst->content += (i + 1) : ft_strclr((char *)lst->content);
-	ft_putstr(*line);
-	return (1);
-}*/
-
 t_list	*ft_getfile(t_list **file, int fd)
 {
+	t_list *tmp;
 
-	if (!*file)
-		MALLOC_CHECK_NULL(*file = ft_lstnew("", fd));
-	while (*file && (*file)->content_size != fd)
+
+	tmp = *file;
+	while (tmp)
+	{
+		if (tmp->content_size == fd)
+		{
+			return (tmp);
+		}
+		tmp = tmp->next;
+	}
+	if (!tmp)
+		MALLOC_CHECK_NULL(tmp = ft_lstnew("", fd));
+	ft_lstadd(file, tmp);
+	tmp = *file;
+
+
+	/*while (*file && (*file)->content_size != fd)
 		*file = (*file)->next;
 	if ((*file)->content_size != fd)
 	{
-		ft_lstadd(file,  ft_lstnew("", fd));
+		ft_lstadd(file, ft_lstnew("", fd));
 		*file = (*file)->next;
 	}
-	return (*file);
+	return (*file);*/
+	return (tmp);
 }
 
 int		get_next_line(const int fd, char **line)
@@ -94,10 +79,11 @@ int		get_next_line(const int fd, char **line)
 	if (fd < 0 || !line || read(fd, buf, 0) < 0)
 		return (-1);
 	lst = ft_getfile(&file, fd);
+	//printf()
 	while ((ret = read(fd, buf, BUF_SIZE)))
 	{
-		if (!(lst->content))
-			MALLOC_CHECK_INT(lst->content = malloc(sizeof(char) * (ret + 1)));
+	//	if (!(lst->content))
+		//	MALLOC_CHECK_INT(lst->content = malloc(sizeof(char) * (ret + 1)));
 		buf[ret] = '\0';
 		temp = (char *)lst->content;
 		lst->content = ft_strjoin((char *)lst->content, buf);
@@ -130,49 +116,38 @@ int		main(int argc, char **argv)
 	int		fd2;
 	char	*line;
 
-
-if (argc < 2)
+	if (argc < 2)
 	{
 		ft_putstr("ERROR");
 		return (0);
 	}
 	if (argc > 2)
 	{
-	fd = open(argv[1], O_RDONLY);
-	fd2 = open(argv[2], O_RDONLY);
-	int a = fd;
-	get_next_line(fd, &line);
-	ft_putstr(line);
-	get_next_line(fd2, &line);
-	ft_putstr(line);
-	get_next_line(fd, &line);
-	ft_putstr(line);
-	get_next_line(fd2, &line);
-	ft_putstr(line);
-	get_next_line(fd, &line);
-	ft_putstr(line);
-	get_next_line(fd2, &line);
-	ft_putstr(line);
-	/*while (get_next_line(a, &line))
-	{
+		fd = open(argv[1], O_RDONLY);
+		fd2 = open(argv[2], O_RDONLY);
+		//printf("%d, %d\n", fd, fd2);
+		get_next_line(fd2, &line);
 		ft_putstr(line);
-		ft_putstr("\n");
-		if (a == fd)
-			a = fd2;
-		else
-			a = fd;
-	}*/
-	}
-else
-{
-	fd = open(argv[1], O_RDONLY);
-	while (get_next_line(fd, &line))
-	{
+		get_next_line(fd2, &line);
 		ft_putstr(line);
-		ft_putstr("\n");
 	}
-}
+	else
+	{
+		fd = open(argv[1], O_RDONLY);
+		while (get_next_line(fd, &line))
+		{
+			ft_putstr(line);
+			ft_putstr("\n");
+		}
+	}
+	free(line);
 
-free(line);
+	/*static t_list *file;
+	t_list *temp;
+	temp = ft_getfile(&file, 1);
+	temp->content = ft_strjoin((char *)temp->content, "TEN");
+	temp = ft_getfile(&file, 4);
+	temp->content = ft_strjoin((char *)temp->content, "ELEVEN");
+	ft_lstprint(file);*/
 	return (0);
 }
